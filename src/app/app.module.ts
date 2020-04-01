6289eea2864121717a8453921c2d4433a189b01c
sergiu-h <sergiu.vivas@gmail.com>
2020-04-01 14:33:05 +0300

    3 files modified, 5 added - hero and message services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
+  bootstrap: [AppComponent],
+  providers: [HeroService, MessageService]
 })
 export class AppModule {}
